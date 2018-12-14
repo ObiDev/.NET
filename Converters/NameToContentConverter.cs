0d7878023260b697397fa9bf2167d4f2d640b1bf
maobi <appskeleton@appskeleton.pl>
2018-12-14 13:51:43 +0100

    Create NameToContentConverter.cs
    Adding NameToContentConverter

---
+++ Converters/NameToContentConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Convert string to UserControl or .cs ViewModel
+
+public class NameToContentConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value != null && value != "")
+        {
+            Type userControl = Type.GetType(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + ".View.MainWindow.UserControls.Details.Custom." + value, null, null);
+            return Activator.CreateInstance(userControl);
+        }
+
+        return "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
